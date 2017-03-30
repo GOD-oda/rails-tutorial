ba41df98fa7271e5e5207db4360020a0b8ad61dc
t.oda <takahiro.tech.oda@gmail.com>
2017-03-30 21:12:17 +0900

    fix: pass all test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get '/articles'
+    assert_response :success
+    assert_not_nil assigns(:articles)
+  end
+
+  test "index should render correct template and layout" do
+    get '/articles'
+    assert_template :index
+    assert_template layout: "layouts/application"
+  end
+
+  test "new should render correct layout" do
+    get '/articles/new'
+    assert_template :new
+    assert_template layout: "layouts/application", partial: "_form"
+  end
 end
